0ad9c23bd2cc2235eb672d196f29556d879b687f
duncup <dlmsoft.wolf@gmail.com>
2019-09-10 12:20:38 +0800

    增加复制到剪切板

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 const alfy = require('alfy');
 const data = await alfy.fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+const rate = '$' + data.bpi.USD.rate;
 
 alfy.output([
 	{
 		title: '当前BTC价格',
-		subtitle: '$' + data.bpi.USD.rate,
-		// arg: data.bpi.USD.rate
+		subtitle: rate,
+		arg: rate
 	}
 ]);
